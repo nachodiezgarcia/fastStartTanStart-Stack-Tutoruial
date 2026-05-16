f885ec62246a3fff85b839532fa7d69f4834b9c0
nachodiezgarcia <diezgarcianacho@gmail.com>
2026-05-16 20:23:49 +0200

    feat: adding tutorial navigate between pages Links

--- tanstart-start-tutorial/src/routes/about.tsx
+++ tanstart-start-tutorial/src/routes/about.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/about')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>
+  return (
+    <>
+      <div>Hello "/about"!</div>
+      <Link to="/">Go to home page!!!</Link>
+    </>
+  )
 }
--- tanstart-start-tutorial/src/routes/index.tsx
+++ tanstart-start-tutorial/src/routes/index.tsx
@@ -1,11 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
     <div className="p-8">
-      <h1>Hello world!!</h1>
+      <h1>Hello world!!</h1>  
+      <Link to='/about'>Go to About page!!!</Link>
     </div>
   )
 }
